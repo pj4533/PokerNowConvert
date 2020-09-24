324a82d25b8d023b495d8fb68f2ae360ee466b6f
PJ Gray <pj@evertrue.com>
2020-09-23 20:20:14 -0400

    Upped limit of log size

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -21,7 +21,7 @@ func routes(_ app: Application) throws {
         ])
     }
 
-    app.on(.POST, "logs", body: .collect(maxSize: "2mb")) { req -> EventLoopFuture<Vapor.View> in
+    app.on(.POST, "logs", body: .collect(maxSize: "4mb")) { req -> EventLoopFuture<Vapor.View> in
         let log = try req.content.decode(Log.self)
 
         var converted : String = ""
